31ac3130656430775c59ac75503b823e3ade09ac
Edimilson Borges <104403198+EdimilsonBorges@users.noreply.github.com>
2023-02-20 20:46:03 -0300

    correcao do icone da pagina
    20-02-23

--- app/Views/html_header.php
+++ app/Views/html_header.php
@@ -9,6 +9,7 @@ defined("ROOT") or die("Acesso negado");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-ico">
     <title>Upper</title>
 
     <?php
--- app/Views/html_login.php
+++ app/Views/html_login.php
@@ -10,6 +10,7 @@ defined("ROOT") or die("Acesso negado");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-ico">
     <title>Tela de login</title>
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
